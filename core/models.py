784319031c93a0aedc26a6ce8c810f0a153855d2
sreyasvm <vmsreyas@gmail.com>
2021-06-21 09:19:39 +0200

    tenant-independent-db (#6)
    * tenant-independent-db
    
    * added migration script
    
    * added dob and changed rating type
    
    * formatting

---
+++ core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=1000)
+    rating = models.DecimalField(null=True, max_digits=3, decimal_places=1)
+    category = models.CharField(max_length=100)
+
+
+class UserData(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    dob = models.DateField(null=True)
+    email = models.EmailField()
+    address = models.CharField(max_length=1000)
+    mobile = models.CharField(max_length=50)
+    photo = models.BinaryField(null=True)
+
+
+class UserLibraryMapping(models.Model):
+    userid = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    libraryid = models.ForeignKey(Library, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
